0b978276abff8422d42dd6f15f7f361a24da9c4a
Felix Langat <fexlangat@gmail.com>
2025-04-25 11:20:34 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+   static Scanner scanner = new Scanner(System.in);
+    static Double balance = 0.00;
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+
+        Double withdraw;
+        Integer choice;
+        boolean isRunning = true;
+
+        while (isRunning){
+
+            System.out.println("ABC BANKING SYSTEM");
+            System.out.println("******************");
+            System.out.println("1. CHECK BALANCE!");
+            System.out.println("2. DEPOSIT!");
+            System.out.println("3. WITHDRAW");
+            System.out.println("4. EXIT");
+
+//            System.out.println("#####################");
+            System.out.println("SELLECT A CHOICE! (1-4)");
+            choice = scanner.nextInt();
+
+            if (choice == 1){
+                showBalance(balance);
+            } else if (choice == 2) {
+                balance += deposit();
+            }else if (choice == 3) {
+                balance -= Withdrawal();
+
+            }else if (choice == 4) {
+                System.out.println("THANKS AND WELCOME AGAIN");
+                isRunning = false;
+
+            }else {
+                System.out.println("INVALID INPUT.");
+            }
         }
+
+scanner.close();
+
     }
+  static   void showBalance(Double balance){
+        System.out.println("******************");
+        System.out.printf("$%.2f", balance);
+
+
+    }
+    static double deposit(){
+       double amaunt;
+       System.out.println("ENTER AMOUNT TO DEPOSIT: ");
+       amaunt = scanner.nextDouble();
+       if (amaunt < 0){
+           System.out.println("ACHA UJINGA");
+       } else {
+           return amaunt;
+       }
+        return amaunt;
+    }
+   static double  Withdrawal(){
+
+        double amaunt;
+        System.out.println("ENTER AMOUNT TO WITHDRAW: ");
+        amaunt = scanner.nextDouble();
+       if (amaunt < balance){
+           System.out.println("TOA PESA");
+       } else if (amaunt > balance) {
+
+           System.out.println("Work harder");
+
+       }
+
+
+       return amaunt;
+   }
+
 }
